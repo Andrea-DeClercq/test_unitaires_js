0b8b2e924981b51c09323c1e6b73a19e33eea489
AndreaDC <andrea.dclercq@gmail.com>
2025-03-13 11:07:02 +0100

    Exo 6 Ajout du cas utilisateur incorrecte, car déjà fait le reste

--- tests/e2e/login.test.js
+++ tests/e2e/login.test.js
@@ -16,3 +16,16 @@ test('Simuler une connexion avec un utilisateur valide', async ({ page }) => {
     await page.click('button[type="submit"]');
     await expect(page).toHaveURL('/dashboard');
 })
+
+test('Simuler une connexion avec un utilisateur invalide', async ({ page }) => {
+    await page.goto('/login');
+
+    await page.fill('input[name="email"]', 'mauvais@user.com'); // Faux identifiant
+    await page.fill('input[name="password"]', 'wrongpassword'); // Faux mot de passe
+
+    await page.click('button[type="submit"]');
+
+    await expect(page).toHaveURL('/login?error=1');
+
+    await expect(page.locator('.error-message')).toHaveText('Identifiants incorrects');
+});
